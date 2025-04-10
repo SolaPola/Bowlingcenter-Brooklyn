bb21f88af44da43db8398a34619551a23c7951ef
ThomasTadesse <328704@student.mboutrecht.nl>
2025-04-10 12:56:18 +0200

    views initial start

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/reservation', [ReservationController::class, 'index'])->name('reservation.index');
+    Route::get('/reservation/create', [ReservationController::class, 'create'])->name('reservation.create');
+    Route::post('/reservation', [ReservationController::class, 'store'])->name('reservation.store');
+    Route::get('/reservation/{id}', [ReservationController::class, 'show'])->name('reservation.show');
+    Route::get('/reservation/{id}/edit', [ReservationController::class, 'edit'])->name('reservation.edit');
+    Route::patch('/reservation/{id}', [ReservationController::class, 'update'])->name('reservation.update');
+    Route::delete('/reservation/{id}', [ReservationController::class, 'destroy'])->name('reservation.destroy');
+
 });
 
 require __DIR__.'/auth.php';
