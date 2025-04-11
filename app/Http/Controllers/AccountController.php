a67816b41cacd779def51f31b6f187c0b96f95e0
ThomasTadesse <328704@student.mboutrecht.nl>
2025-04-11 11:32:38 +0200

    accounts CRUD initial start

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Person;
+use App\Models\Contact;
+
+
+class AccountController extends Controller
+{
+    //
+}
