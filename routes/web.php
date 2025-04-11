d96460e82813b0fe203214e68bd16aa7536eacdb
ThomasTadesse <328704@student.mboutrecht.nl>
2025-04-11 11:29:41 +0200

    Routing accounts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ReservationController;
 
 Route::get('/', function () {
@@ -18,27 +20,34 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
- // Use the resource route which defines all CRUD routes
- Route::resource('reservations', ReservationController::class);
-    Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
-    Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
-    Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
-    Route::get('/reservations/{id}', [ReservationController::class, 'show'])->name('reservations.show');
-    Route::get('/reservations/{id}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
-    Route::put('/reservations/{id}', [ReservationController::class, 'update'])->name('reservations.update');
-    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
-
-
 
 
 // Reservation routes with auth middleware
-Route::middleware(['auth'])->group(function () {
-   
-    
+Route::middleware(['auth'])->group(function () {    
     // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+     // Use the resource route which defines all CRUD routes
+    Route::resource('reservations', ReservationController::class);
+        Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+        Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
+        Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
+        Route::get('/reservations/{id}', [ReservationController::class, 'show'])->name('reservations.show');
+        Route::get('/reservations/{id}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
+        Route::put('/reservations/{id}', [ReservationController::class, 'update'])->name('reservations.update');
+        Route::delete('/reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
+
+    // use the resource route which defines all CRUD routes
+    Route::resource('accounts', OrderController::class);
+        Route::get('/accounts', [OrderController::class, 'index'])->name('accounts.index');
+        Route::get('/accounts/create', [OrderController::class, 'create'])->name('accounts.create');
+        Route::post('/accounts', [OrderController::class, 'store'])->name('accounts.store');
+        Route::get('/accounts/{id}', [OrderController::class, 'show'])->name('accounts.show');
+        Route::get('/accounts/{id}/edit', [OrderController::class, 'edit'])->name('accounts.edit');
+        Route::put('/accounts/{id}', [OrderController::class, 'update'])->name('accounts.update');
+        Route::delete('/accounts/{id}', [OrderController::class, 'destroy'])->name('accounts.destroy');
 });
 Route::middleware(['auth'])->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
@@ -49,13 +58,6 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->name('users.delete');
 });
 
-
-    Route::get('/order', [OrderController::class, 'index'])->name('order.index');
-    Route::post('/order', [OrderController::class, 'store'])->name('order.store');
-    Route::put('/order/{id}', [OrderController::class, 'update'])->name('order.update');
-    Route::delete('/order/{id}', [OrderController::class, 'destroy'])->name('order.destroy');
-    Route::get('/order/{id}/edit', [OrderController::class, 'edit'])->name('order.edit');
-    Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
 
 
 
