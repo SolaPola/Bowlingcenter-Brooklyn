5849b924be3a5f38a6672727450bf2588f1a2b52
ThomasTadesse <328704@student.mboutrecht.nl>
2025-04-11 12:47:33 +0200

    routes misc fix

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,17 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/accounts/{id}/edit', [OrderController::class, 'edit'])->name('accounts.edit');
         Route::put('/accounts/{id}', [OrderController::class, 'update'])->name('accounts.update');
         Route::delete('/accounts/{id}', [OrderController::class, 'destroy'])->name('accounts.destroy');
-});
+
+
+    // use the resource route which defines all CRUD routes
+        Route::get('/order', [OrderController::class, 'index'])->name('order.index');
+        Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+        Route::put('/order/{id}', [OrderController::class, 'update'])->name('order.update');
+        Route::delete('/order/{id}', [OrderController::class, 'destroy'])->name('order.destroy');
+        Route::get('/order/{id}/edit', [OrderController::class, 'edit'])->name('order.edit');
+        Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
+    });
+    
 Route::middleware(['auth'])->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
@@ -56,6 +66,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
     Route::post('/users/update/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->name('users.delete');
+
+
+
 });
 
 
