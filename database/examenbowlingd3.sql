07e7be2325494c1905a01706a48fbf0d14ceff54
LorenzoABS <150017251+LorenzoABS@users.noreply.github.com>
2025-04-11 10:55:34 +0200

    sp gemaakt en test data toegevoegt in de db

--- database/examenbowlingd3.sql
+++ database/examenbowlingd3.sql
@@ -39,6 +39,17 @@ CREATE TABLE IF NOT EXISTS `persoon` (
   PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- Testdata voor tabel `persoon`
+INSERT INTO `persoon` (`Id`, `TypePersoon`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Roepnaam`, `IsVolwassen`) VALUES
+(1, 'Klant', 'Mazin', NULL, 'Jamil', 'Mazin', 1),
+(2, 'Klant', 'Arjan', 'de', 'Ruijter', 'Arjan', 1),
+(3, 'Klant', 'Hans', NULL, 'Odijk', 'Hans', 1),
+(4, 'Klant', 'Dennis', 'van', 'Wakeren', 'Dennis', 1),
+(5, 'Medewerker', 'Wilco', 'Van de', 'Grift', NULL, 1),
+(6, 'Gast', 'Tom', NULL, 'Sanders', NULL, 0),
+(7, 'Gast', 'Andrew', NULL, 'Sanders', NULL, 0),
+(8, 'Gast', 'Julian', NULL, 'Kaldenheuvel', NULL, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +74,39 @@ CREATE TABLE IF NOT EXISTS `reservering` (
   PRIMARY KEY (`Id`),
   KEY `PersoonId` (`PersoonId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Testdata voor tabel `reservering`
+INSERT INTO `reservering` (`Id`, `PersoonId`, `OpeningstijdId`, `BaanId`, `PakketOptieId`, `ReserveringStatus`, `Reserveringsnummer`, `Datum`, `AantalUren`, `BeginTijd`, `EindTijd`, `AantalVolwassen`, `AantalKinderen`) VALUES
+(1, 1, 2, 8, 1, 'Bevestigd', '2022122000001', '2022-12-20', 1, '15:00', '16:00', 4, 2),
+(2, 2, 2, 8, 1, 'Bevestigd', '2022122000002', '2022-12-20', 1, '17:00', '18:00', 4, NULL),
+(3, 3, 2, 8, 3, 'Bevestigd', '2022122400003', '2022-12-24', 2, '16:00', '18:00', 4, NULL),
+(4, 4, 2, 8, NULL, 'Bevestigd', '2022122700004', '2022-12-27', 2, '17:00', '19:00', 2, NULL),
+(5, 5, 2, 8, 1, 'Bevestigd', '2022122800005', '2022-12-28', 1, '14:00', '15:00', 3, NULL),
+(6, 5, 2, 8, 1, 'Bevestigd', '2022122800006', '2022-12-28', 2, '19:00', '21:00', 2, NULL);
+
+-- DELIMITER $$
+
+-- CREATE PROCEDURE GetReserveringOverzicht()
+-- BEGIN
+--     SELECT 
+--         p.Voornaam,
+--         p.Tussenvoegsel,
+--         p.Achternaam,
+--         r.Datum,
+--         r.AantalUren,
+--         r.AantalVolwassen,
+--         r.AantalKinderen,
+--         r.ReserveringStatus
+--     FROM 
+--         persoon p
+--     INNER JOIN 
+--         reservering r
+--     ON 
+--         p.Id = r.PersoonId;
+-- END$$
+
+-- DELIMITER ;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ database/migrations/2025_04_11_084000_create_get_reservering_overzicht_sp.php
@@ -0,0 +1,45 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateGetReserveringOverzichtSp extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+            CREATE PROCEDURE GetReserveringOverzicht()
+            BEGIN
+                SELECT 
+                    p.Voornaam,
+                    p.Tussenvoegsel,
+                    p.Achternaam,
+                    r.Datum,
+                    r.AantalUren,
+                    r.AantalVolwassen,
+                    r.AantalKinderen,
+                    r.ReserveringStatus
+                FROM 
+                    persoon p
+                INNER JOIN 
+                    reservering r
+                ON 
+                    p.Id = r.PersoonId;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP PROCEDURE IF EXISTS GetReserveringOverzicht');
+    }
+}
